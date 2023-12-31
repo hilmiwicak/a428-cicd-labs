41ba7b2c0ffb2ad10e76360ba0941141458e8fe6
hilmiwicak <hilmiwicaksonop@gmail.com>
2023-12-31 13:17:26 +0700

    feat: edit react app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -9,6 +9,9 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React!</h1>
+          <h2>
+            This is a repo made for Dicoding's course : "Implementasi CICD"
+          </h2>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
